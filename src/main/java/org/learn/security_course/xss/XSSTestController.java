1f34edb357cf281dc3403d6388470bc4a4930f3d
sadiul-hakim <sadiulhakim@gmail.com>
2024-06-23 13:42:18 +0600

    XSS

--- src/main/java/org/learn/security_course/xss/XSSTestController.java
+++ src/main/java/org/learn/security_course/xss/XSSTestController.java
@@ -1,18 +1,22 @@
 package org.learn.security_course.xss;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/xss/danger/api")
+@Validated
 public class XSSTestController {
 
     @CrossOrigin("*")
     @GetMapping(value = "/greet/{name}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> greet(@PathVariable String name) {
+    public ResponseEntity<?> greet(@PathVariable @Valid @Pattern(regexp = "[A-Za-z]{5,20}") String name) {
         return ResponseEntity.ok("Hello, " + name);
     }
 
